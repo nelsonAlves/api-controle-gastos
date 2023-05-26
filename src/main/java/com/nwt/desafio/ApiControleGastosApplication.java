796a1d01dadaa3959f220860501069e219fa15b3
nelsonAlves <nelsonalves.olv@gmail.com>
2023-05-25 21:43:21 -0300

    Initial commit

---
+++ src/main/java/com/nwt/desafio/ApiControleGastosApplication.java
@@ -0,0 +1,13 @@
+package com.nwt.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleGastosApplication.class, args);
+	}
+
+}
