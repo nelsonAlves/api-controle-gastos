fa9d58b893c0b6e5c3ebe026965374cfd58def99
nelsonAlves <nelsonalves.olv@gmail.com>
2023-05-30 22:09:00 -0300

    Alteração do arquivo .properties para criação de profile pra H2 e arquivo Import.sql para carregamento H2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_centro_custo (descricao, observacao) VALUES ('Centro custo 1', 'Teste 1');
+INSERT INTO tb_centro_custo (descricao, observacao) VALUES ('Centro custo 2', 'Teste 2');
+INSERT INTO tb_centro_custo (descricao, observacao) VALUES ('Centro custo 3', 'Teste 3');
+
+INSERT INTO tb_usuario (data_cadastro, data_inativacao, email, foto, nome, senha) VALUES('2023-05-12 00:42:29', '', 'teste1@gmail.com', '', '', '123');
+INSERT INTO tb_usuario (data_cadastro, data_inativacao, email, foto, nome, senha) VALUES('2023-05-12 00:42:29', '', 'teste2@gmail.com', '', '', '1234');
+INSERT INTO tb_usuario (data_cadastro, data_inativacao, email, foto, nome, senha) VALUES('2023-05-12 00:42:29', '', 'teste3@gmail.com', '', '', '12345');
+
+INSERT INTO tb_titulo (data_cadastro, data_pagamento, data_referencia, data_vencimento, descricao, observacao, tipo, id_centro_custo, id_usuario) VALUES('2023-05-12 00:42:29', '', '', '', '', '', 0, 0, 0);
+INSERT INTO tb_titulo (data_cadastro, data_pagamento, data_referencia, data_vencimento, descricao, observacao, tipo, id_centro_custo, id_usuario) VALUES('2023-05-12 00:42:29', '', '', '', '', '', 0, 0, 0);
+INSERT INTO tb_titulo (data_cadastro, data_pagamento, data_referencia, data_vencimento, descricao, observacao, tipo, id_centro_custo, id_usuario) VALUES('2023-05-12 00:42:29', '', '', '', '', '', 0, 0, 0);
+
